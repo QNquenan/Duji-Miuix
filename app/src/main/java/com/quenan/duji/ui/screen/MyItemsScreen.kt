52a959db02b326d0f2eabce73b6915ed602da27c
QNquenan <qn2987271942@outlook.com>
2026-07-04 17:51:58 +0800

    修正 FAB 隐藏方向

--- app/src/main/java/com/quenan/duji/ui/screen/MyItemsScreen.kt
+++ app/src/main/java/com/quenan/duji/ui/screen/MyItemsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Spacer
@@ -202,7 +203,7 @@ fun MyItemsScreen(
                     .navigationBarsPadding()
                     .padding(contentPadding)
                     .padding(end = 16.dp, bottom = 16.dp)
-                    .padding(bottom = fabBottomOffset),
+                    .offset(y = fabBottomOffset),
                 onClick = { showBottomSheet = true },
             ) {
                 Icon(
