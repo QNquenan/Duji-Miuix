b4f2a42f24007bbcb16abe89107436c6e145ecac
QNquenan <qn2987271942@outlook.com>
2026-07-21 16:27:48 +0800

    feat: 发布0.11.2版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.quenan.duji"
         minSdk = 33
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.11.1"
+        versionCode = 26
+        versionName = "0.11.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
